922ff7ef2ad65e291621550e960a2966990a4cf0
theGrandmaster05 <israelakintunde005@gmail.com>
2018-12-17 01:46:29 +0100

    Setting up the Footer component

---
+++ app/components/partials/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <footer className="footer">
+                <div>
+                    <p>Developed by <a href="https://www.twitter.com/theGrandmastar" target="_blank"> theGrandmaster</a>
+                    </p>
+                </div>
+            </footer>
+        )
+    }
+}
+
+export default Footer;
--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './static/css/index.css'
 import App from './components/App';
+import './static/css/index.css';
 
 ReactDOM.render(
     <App />,
