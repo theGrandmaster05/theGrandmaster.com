3829deb555105ee6901616eab8203cc78e923053
theGrandmaster05 <israelakintunde005@gmail.com>
2018-12-18 02:14:07 +0100

    Completing the blog posts display page

--- app/components/Blog.js
+++ app/components/Blog.js
@@ -6,8 +6,22 @@ class Blog extends React.Component {
             <div className="blog-posts-page">
                 <div className="single-post">
                     <h4 className="title">
-                        Red Head
+                        Using the aria-live attribute on HTML elements
                     </h4>
+                    <span className="date">
+                        <a href="">
+                            theGrandmaster
+                        </a> - Dec 17, 2019 - #tags
+                    </span>
+                    <p className="snippet">
+                        Actually, a lot of the things im going to be writing here are jibber jabber, so please dont put all of your mind into this part of the code at least for now
+                        and i could just have copied and pasted what i have above but, actually whats the point of that?, well if you ask me i will tell you that i dont 
+                        know, why do we even do things <a href="">read more 
+                            <svg>
+
+                            </svg>
+                        </a>
+                    </p>
                 </div>
             </div>
         )
